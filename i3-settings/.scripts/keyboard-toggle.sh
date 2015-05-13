ebdb3629b5aa57c5f9c5d4264aaf3fb206d3fff3
Jacob Wortmann <jacob@wortmann.dk>
2015-05-13 11:22:11 +0200

    new way to change the keyboard settings

---
+++ i3-settings/.scripts/keyboard-toggle.sh
@@ -0,0 +1,15 @@
+#! /bin/bash 
+file="/home/jacob/.i3/.scripts/keyboard-setting.txt"
+
+if [ ! -f "$file" ]; then
+    echo "dk" > $file; 
+fi
+
+key=$(cat "$file")
+if [ $key == "gb" ]; then
+    setxkbmap dk
+    echo "dk" > $file
+else
+    setxkbmap gb
+    echo "gb" > $file
+fi
